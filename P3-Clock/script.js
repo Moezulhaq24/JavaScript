b91a4634eb0a79239bee4f029e0b26c0e3f1b219
Moez Ul Haq <161411971+Moezulhaq24@users.noreply.github.com>
2026-06-29 15:18:23 +0500

    Add files via upload

---
+++ P3-Clock/script.js
@@ -0,0 +1,12 @@
+// const clock = document.querySelector("#clock")
+const clock = document.getElementById("clock")
+// console.log(clock)
+
+
+
+setInterval(function(){
+    let date = new Date()
+    // console.log(date.toLocaleTimeString()) 
+    clock.innerHTML = date.toLocaleTimeString()
+    
+},1000)
